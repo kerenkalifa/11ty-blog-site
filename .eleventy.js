44582b08c8617e2869779fadbc7b5419a4ef2c16
Keren Kalifa <kerenkalifa@IL-KERENKA-VES.lan>
2021-09-28 21:31:01 +0300

    fixed admin pannel

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
   eleventyConfig.addFilter("postDate", (dateObject) => {
     return DateTime.fromJSDate(dateObject).toLocaleString(DateTime.DATE_MED);
